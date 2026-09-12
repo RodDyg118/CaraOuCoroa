a4b6bd0c112737e7d79ce179aa4e2ce5bc935509
RodDyg118 <rodresscoldskj@gmail.com>
2026-09-11 22:14:32 -0300

    CaraOuCoroa

---
+++ CaraOuCoroa/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace CaraOuCoroa
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GirarMoeda(object? sender, EventArgs e)
+        {
+            string EscolhaDoUsuario = BotaoListaOpcoes.SelectedItem.ToString();
+
+
+            Random Moeda = new Random();
+            int LadoSorteado = Moeda.Next(2);
+
+        }
+
+        private void OpcaoSelecionadaPicker(object? sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
